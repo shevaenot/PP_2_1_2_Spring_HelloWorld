a13338c2b28ab3a3179569e2d43c0cba4810f332
enoti <enotishe314@gmail.com>
2023-03-12 11:59:53 +0300

    fd

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (bean == bean2));
+        System.out.println(bean);
+        System.out.println(bean2);
+
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (cat1 == cat2));
+        System.out.println(cat1);
+        System.out.println(cat2);
     }
 }
